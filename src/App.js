4c87f17643a3bd163b783100864a1a80ff8eda90
SM-tek <96948516+smtekdev@users.noreply.github.com>
2023-06-28 10:58:28 +0500

    complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { Ground } from './Ground';
+import { Canvas } from '@react-three/fiber';
+import "./style.css";
+import { OrbitControls, PerspectiveCamera, useGLTF, Environment} from '@react-three/drei';
 
+function CarShow(){
+  const guit = useGLTF("./Guitar-Case.glb");
+  const wood = useGLTF("./wooden.glb");
+  // const electric = useGLTF("./electric.glb");
+  return(
+    <>
+    <OrbitControls  target={[0, 0.35, 0]} maxPolarAngle={1.45}  />
+    <primitive object={guit.scene} position-y={-0.1} position-z={1} scale={0.4} />
+    <primitive object={wood.scene} position-x={-1.6} scale={0.5} />
+    {/* <primitive object={electric.scene} scale={0.5} /> */}
+    <PerspectiveCamera makeDefault fov={50} position={[3,2,5]} />
+    <color args={[0, 0, 0]} attach="background" />
+    <Environment preset={"city"} />
+    <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+       <Ground />
+       
+    </>
+  )
+}
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={null}>
+      <Canvas shadows>
+        <CarShow />
+      </Canvas>
+    </Suspense>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
 import App from './App';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
